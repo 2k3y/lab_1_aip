fa53f1384bde3d3bb2f060c5a74ffb457835b8c0
Kirill Semiletov <Kirillsemiletov06@gmail.com>
2025-09-18 20:40:26 +0300

    Initial commit

---
+++ src/ATC.java
@@ -0,0 +1,60 @@
+public class ATC {
+
+    private String address;
+    private int subscribersCount;
+    private double monthlyFee; // будет свойство *только для записи*
+
+    public ATC(String address, int subscribersCount, double monthlyFee) {
+        setAddress(address);
+        setSubscribersCount(subscribersCount);
+        setMonthlyFee(monthlyFee);
+    }
+
+    public boolean setAddress(String address) {
+        if (address == null || address.isBlank()) {
+            System.out.println("Ошибка: адрес пустой!");
+            return false;
+        }
+        this.address = address;
+        return true;
+    }
+
+
+    public void setSubscribersCount(int subscribersCount) {
+        if (subscribersCount < 0) {
+            System.out.println("Введено отрицательное число абонентов, установлено значение 0");
+            this.subscribersCount = 0;
+        } else {
+            this.subscribersCount = subscribersCount;
+        }
+    }
+
+
+    public void setMonthlyFee(double monthlyFee) {
+        if (monthlyFee < 0) {
+            System.out.println("Ошибка: абонентская плата не может быть отрицательной!");
+        } else {
+            this.monthlyFee = monthlyFee;
+        }
+    }
+
+
+    public double computeTotalMonthlyFee() {
+        return monthlyFee * subscribersCount;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getSubscribersCount() {
+        return subscribersCount;
+    }
+
+    @Override
+    public String toString() {
+        return "ATC по адресу: " + address + ", кол-во абонентов: " + subscribersCount;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Создание объекта с нормальными данными
+        ATC atc = new ATC("СПб, ул. Связи, 1", 1200, 300.0);
+
+        // 1. Вывод объекта (через toString)
+        System.out.println(atc);
+
+        // 2. Подсчёт общей абонентской платы
+        double total = atc.computeTotalMonthlyFee();
+        System.out.println("Общая абонентская плата: " + total);
+
+        // 3. Проверка ошибок
+        atc.setSubscribersCount(-10);  // должно вывести предупреждение
+        atc.setMonthlyFee(-5);         // должно вывести предупреждение
+        boolean ok = atc.setAddress("qwe"); // false + сообщение
+
+        // 4. Ещё раз выводим объект и итог
+        System.out.println(atc);
+        System.out.println("Общая абонентская плата после ошибок: " + atc.computeTotalMonthlyFee());
+    }
+}
